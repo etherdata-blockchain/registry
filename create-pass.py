972aacf301822aab4012a0c68fc9529ced02c2c5
sirily11 <sirily1997@gmail.com>
2022-02-09 00:32:10 +0800

    first commit

---
+++ create-pass.py
@@ -0,0 +1,55 @@
+import os
+from subprocess import run
+
+
+# run docker create password command
+def create_pass(username, password):
+    # run htpasswd command
+    # docker run --entrypoint htpasswd httpd:2 -Bbn testuser testpassword > auth/htpasswd
+    cmd = ["docker", "run", "--entrypoint", "htpasswd",
+           "httpd:2", "-Bbn", username, password]
+
+    # run command
+    run(cmd)
+
+
+# create auth folder if not exist
+def create_auth_folder():
+    # run command
+    run(["mkdir", "-p", "auth"])
+
+
+# check if htpasswd file exist, and return True if exist
+def check_htpasswd_file():
+    if os.path.isfile("./auth/htpasswd"):
+        return True
+    return False
+
+# get username and password from user
+
+
+def get_username_password():
+    username = input("Username: ")
+    password = input("Password: ")
+    return username, password
+
+
+# main function
+def main():
+    # create auth folder if not exist
+    create_auth_folder()
+
+    # create htpasswd file if not exist
+    if not check_htpasswd_file():
+        username, password = get_username_password()
+        create_pass(username, password)
+    else:
+        print("File already exist")
+        # delete file if user want to overwrite
+        if input("Do you want to overwrite? (y/n): ") == "y":
+            username, password = get_username_password()
+            create_pass(username, password)
+
+
+if __name__ == "__main__":
+    main()
